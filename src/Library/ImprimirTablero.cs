c23bbca6d396138af491999f11941c49d3baba5c
Juan Riccetto <juanmartinre@gmail.com>
2021-09-07 11:51:36 -0300

    repo

---
+++ src/Library/ImprimirTablero.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace PII_Game_Of_Life
+{
+    public class ImprimirTablero
+    {
+        public void Imprimir(Gameboard gameboard)
+        {
+            int width = gameboard.GameboardState.GetLength(0); //variabe que representa el ancho del tablero
+            int height = gameboard.GameboardState.GetLength(1);//variabe que representa altura del tablero
+            while (true)
+            {
+                Console.Clear();
+                StringBuilder s = new StringBuilder();
+                for (int y = 0; y<height;y++)
+                {
+                    for (int x = 0; x<width; x++)
+                    {
+                        if(gameboard.GameboardState[x,y])
+                        {
+                            s.Append("|X|");
+                        }
+                        else
+                        {
+                            s.Append("___");
+                        }
+                    }
+                    s.Append("\n");
+                }
+                Console.WriteLine(s.ToString());
+                //=================================================
+                //Invocar método para calcular siguiente generación
+                //=================================================
+                Thread.Sleep(300);
+            }
+        }
+    }
+} 
---
+++ src/Program/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace PII_Game_Of_Life
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Gameboard gameboard = new Gameboard();
+
+
+            bool[,] gameBoard = /* contenido del tablero */;
+            int boardWidth = gameBoard.GetLength(0);
+            int boardHeight = gameBoard.GetLength(1);
+        }
+    }
+}
